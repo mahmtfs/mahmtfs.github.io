ecdfcab3337ed4d67906572c75566627ab5c7d42
Rinat Mynnakhmetov <rinnik228@gmail.com>
2024-08-19 00:55:47 -0400

    Added dark mode

---
+++ js/theme_switch.js
@@ -0,0 +1,24 @@
+var theme = parseInt(localStorage.getItem("theme"));
+
+if (!theme){
+    theme = 1;
+}
+
+function themeSetter(){
+    if (theme){
+        document.getElementById("dark_mode_status").innerHTML = "ON";
+        document.getElementById("dark_mode").className = "btn border-light rounded-0";
+        document.getElementsByTagName("html")[0].setAttribute("data-bs-theme", "dark");
+    }
+    else{
+        document.getElementById("dark_mode_status").innerHTML = "OFF";
+        document.getElementById("dark_mode").className = "btn border-dark rounded-0";
+        document.getElementsByTagName("html")[0].setAttribute("data-bs-theme", "light");
+    }
+}
+
+function themeSwitch() {
+    theme = (theme + 1) % 2;
+    localStorage.setItem("theme", theme);
+    themeSetter();
+}
